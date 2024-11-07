c94a6dd6effb09b45fa6bd2d3dd0c65bd6266d2a
Nhat Thai <100763926+nhaticus@users.noreply.github.com>
2024-11-07 10:49:19 -0800

    getting started

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 // todo
+import "./style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const APP_NAME = "Geocoin";
+document.title = APP_NAME;
+
+function createButton(
+  buttonText: string,
+  eventHandler: () => void,
+): HTMLButtonElement {
+  const tmpButton = document.createElement("button");
+  tmpButton.innerHTML = buttonText;
+  tmpButton.style.color = "white";
+  tmpButton.addEventListener("click", eventHandler);
+  app.appendChild(tmpButton);
+  return tmpButton;
+}
+
+const button = createButton("click me!", () => {
+  alert("WOW! you've clicked a button");
+});
+app.append(button);
